70e784914f7bd9fc751366237e4c437d48bb8f21
AlperK <alperkilic93@gmail.com>
2023-11-29 15:54:05 -0500

    Data from November 16

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def s_ph(u_a, u_s, f):
     return temp**0.5
 
 
-measurementPath = Path("2023-11-16", "Arm-120-60-120-60mmHg", "3")
+measurementPath = Path("2023-11-20", "ARTERIAL", "7")
 measurement1 = Measurements.DualSlopePhantomMeasurement(location=measurementPath,
                                                         common='detector')
 measurement2 = Measurements.DualSlopePhantomMeasurement(location=measurementPath,
@@ -47,7 +47,7 @@ absorption_coefficient_830 = fdNIRS.compute_optical_parameters(
     measurement1.dual_amplitude_slopes_color1,
     measurement1.dual_phase_slopes_color1,
     measurement1.modulation_frequency)[0].mean(axis=1)
-absorption_coefficient_685 = fdNIRS.compute_optical_parameters(
+absorption_coefficient_685 = -fdNIRS.compute_optical_parameters(
     measurement1.dual_amplitude_slopes_color2,
     measurement1.dual_phase_slopes_color2,
     measurement1.modulation_frequency)[0].mean(axis=1)
@@ -55,30 +55,39 @@ print('------------------------------')
 # print(fdNIRS.compute_hemoglobins(absorption_coefficient_830,
 #                                  absorption_coefficient_685))
 # print('------------------------------')
+s = absorption_coefficient_830.size // 10
+
 absorption_coefficient_830 = fdNIRS.rolling_apply(np.mean, a=absorption_coefficient_830, w=15)
 absorption_coefficient_685 = fdNIRS.rolling_apply(np.mean, a=absorption_coefficient_685, w=15)
 HbO, HbR = fdNIRS.compute_hemoglobin_concentrations(absorption_coefficient_830,
                                                     absorption_coefficient_685)
-print(fdNIRS.compute_hemoglobin_concentrations(absorption_coefficient_830,
-                                               absorption_coefficient_685).mean(axis=1))
+# print(fdNIRS.compute_hemoglobin_concentrations(absorption_coefficient_830,
+#                                                absorption_coefficient_685).mean(axis=1))
 print('------------------------------')
-
+print(HbO.size)
+t = np.linspace(0, 300, HbO.size)
 plt.figure()
 plt.suptitle('Absorption coefficients')
-plt.plot(absorption_coefficient_830, label='830nm')
-plt.plot(absorption_coefficient_685, label='685nm')
-plt.axvline(83)
-plt.axvline(127)
-plt.axvspan(83, 127, alpha=0.15, color='blue')
+plt.plot(t, absorption_coefficient_830, label='830nm', linewidth=3, alpha=0.75)
+plt.plot(t, absorption_coefficient_685, label='685nm', linewidth=3, alpha=0.75)
+plt.xlabel('Time(s)')
+plt.ylabel(r'$mm^{-1}$')
+# plt.tick_params(axis='x', bottom=False, labelbottom=False)
+# plt.axvline(s*4)
+# plt.axvline(s*7)
+plt.axvspan(120, 180, alpha=0.15, color='blue', label='occlusion')
 plt.legend()
 
 plt.figure()
 plt.suptitle('Hemoglobin concentrations')
-plt.plot(fdNIRS.rolling_apply(np.mean, a=HbO, w=5), label='HbO')
-plt.plot(fdNIRS.rolling_apply(np.mean, a=HbR, w=5), label='HbR')
-plt.axvline(83)
-plt.axvline(127)
-plt.axvspan(83, 127, alpha=0.15, color='blue')
+plt.plot(t, 1e6 * fdNIRS.rolling_apply(np.mean, a=HbO, w=1), label='HbO', linewidth=3, alpha=0.75)
+plt.plot(t, 1e6 * fdNIRS.rolling_apply(np.mean, a=HbR, w=1), label='HbR', linewidth=3, alpha=0.75)
+plt.plot(t, 1e6 * fdNIRS.rolling_apply(np.mean, a=HbR+HbO, w=1), label='HbT', linewidth=3, alpha=0.75)
+plt.xlabel('Time(s)')
+plt.ylabel(r'$\mu$M $mm^{-2}$')
+# plt.axvline(s*4)
+# plt.axvline(s*7)
+plt.axvspan(120, 180, alpha=0.15, color='blue', label='occlusion')
 plt.legend()
 
 plt.show()
